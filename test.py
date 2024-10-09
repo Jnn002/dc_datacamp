a1ea149671e1082f318ec74d1b9891092f389099
John <jyaxp1@miumg.edu.gt>
2024-10-08 22:43:46 -0600

    ADD, testing file

---
+++ test.py
@@ -0,0 +1,4 @@
+customer_revies = ['I love this phone', 'I hate', 'Everything is good', 'Good', 'Bad', 'Good', 'Bad', 'Bad', 'Good', 'Good', 'Good', 'Bad', 'Bad']
+
+# last 5 reviews
+last_review = customer_revies[-5:]
