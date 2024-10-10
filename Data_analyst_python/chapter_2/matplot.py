5cca6e0080b80c1a18953e57b4286622b05aaa99
John <jyaxp1@miumg.edu.gt>
2024-10-09 20:24:18 -0600

    Change, matplot file

--- Data_analyst_python/chapter_2/matplot.py
+++ Data_analyst_python/chapter_2/matplot.py
@@ -4,3 +4,9 @@ year = [1950, 1970, 1990, 2010]
 pop = [2.519, 3.692, 5.263, 6.972]
 plt.plot(year, pop)
 plt.show()
+
+# Histogram
+values = [0, 0.6, 1.4, 1.6, 2.2, 2.5, 2.6, 3.2, 3.5, 3.9, 4.2]
+
+plt.hist(values, bins=3)
+plt.show
