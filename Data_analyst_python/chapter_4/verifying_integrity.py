c02d9eabb03de84af56d59cd2e0d2e4be7e6e6c9
John <jyaxp1@miumg.edu.gt>
2024-10-17 22:00:31 -0600

    ADD, verification of integrity for merge() and concat methods

---
+++ Data_analyst_python/chapter_4/verifying_integrity.py
@@ -0,0 +1,15 @@
+# IN python we can verify the integrity of the data by checking 
+# for missing values, duplicate data, and outliers.
+
+# For duplicate values in merge() method, we can use the validate 
+# argument to check if the data is one-to-one, one-to-many, or many-to-many.
+
+# For missing values, we can use the isnull() method to check for missing values.
+
+# For outliers, we can use the z-score to detect outliers.
+
+
+#* If we talk about concatenation, we can use the ignore_index argument to
+#* reset the index of the resulting DataFrame.
+
+# and verify_integrity argument to check if the new DataFrame has any duplicate values
